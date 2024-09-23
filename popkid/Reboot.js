34bc8f7ca7041fbf22f72f83808aa449c4fc4f59
Popkid-md <weanslatevanessa@gmail.com>
2024-09-23 16:41:50 +0300

    Popkid😈

--- popkid/Reboot.js
+++ popkid/Reboot.js
@@ -6,7 +6,7 @@ const {zokou}=require("../framework/zokou")
 
 
 
-zokou({nomCom:"restart",categorie:"Mods",reaction:"📴"},async(dest,z,com)=>{
+zokou({nomCom:"restart",categorie:"Mods",reaction:"🐥"},async(dest,z,com)=>{
 
 
   
@@ -14,12 +14,12 @@ const{repondre,ms,dev,superUser}=com;
 
   if(!superUser)
   {
-    return repondre("This command is for owner or joel_it");
+    return repondre("This command is for owner or popkid");
   }
 
   const {exec}=require("child_process")
 
-    repondre("𝙹𝙾𝚎𝚕 𝚖𝚍 𝚒𝚜 𝚛𝚎𝚜𝚝𝚊𝚛𝚝𝚒𝚗𝚐..");
+    repondre("popkid 𝚖𝚍 𝚒𝚜 𝚛𝚎𝚜𝚝𝚊𝚛𝚝𝚒𝚗𝚐..");
 
   exec("pm2 restart all");
   
